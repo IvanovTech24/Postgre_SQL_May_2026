4b6c2223144cdf18296ce52f8ba87f42b6c3c09d
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-06-09 18:40:40 +0300

    feat: using JOIN with USING

---
+++ 11_Subqueries_and_JOINs_Lab_02_06_2026/03_employees_projects.sql
@@ -0,0 +1,17 @@
+SELECT
+    e.employee_id,
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    p.project_id,
+    p.name
+FROM
+    employees AS e
+JOIN
+    employees_projects AS ep
+USING
+    (employee_id)
+JOIN
+    projects AS p
+USING
+    (project_id)
+WHERE
+    p.project_id = 1;
