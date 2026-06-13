b76fda03971fcfe2f001a6d7ff05865cae0be668
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-06-13 12:46:04 +0300

    feat: create INSERT, UPDATE, DELETE queries

---
+++ 17_Regular_Exam_13_06_2026/2_4_delete.sql
@@ -0,0 +1,14 @@
+DELETE FROM customers
+WHERE
+    id IN (
+SELECT
+    c.id
+FROM
+    customers AS c
+LEFT JOIN
+    orders AS o
+ON
+    c.id = o.customer_id
+WHERE
+    o.customer_id IS NULL)
+RETURNING *;
