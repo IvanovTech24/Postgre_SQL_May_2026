88748cf9848019573174ba71e09e7ffe986cbd4e
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-06-24 20:13:07 +0300

    feat: create function and procedure

---
+++ 21_Exam_Preparation_I/4_12_animals_with_owner_or_not.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+    IN animal_name VARCHAR(30),
+    OUT owner_name TEXT
+) AS
+$$
+BEGIN
+    SELECT
+        COALESCE(o.name, 'For adoption')
+    INTO
+        owner_name
+    FROM
+        animals AS a
+    LEFT JOIN
+        owners AS o
+    ON
+        a.owner_id = o.id
+    WHERE
+        a.name = animal_name;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+SELECT
+    *
+FROM
+    animals AS a
+JOIN
+    owners AS o
+ON
+    a.owner_id = o.id
+-- WHERE
+--     o.id IS NULL;
